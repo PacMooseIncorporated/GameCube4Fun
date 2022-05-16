ede0c276746a6faa149ee88c8aeb0042b8a89280
shazz <shazz.ng@gmail.com>
2022-05-16 10:17:04 -0400

    added devoptab

--- libogc2/console/include/bba_debug.h
+++ libogc2/console/include/bba_debug.h
@@ -1,6 +1,10 @@
 #ifndef __BBA_DEBUG_H
 #define __BBA_DEBUG_H
 
+// devoptab only
+int enable_bba_traces();
+
+// custom
 int configure_bba();
 int open_udp_socket(int port, char * server_ip);
 void close_udp_socket();
